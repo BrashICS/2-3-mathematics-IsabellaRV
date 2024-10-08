e5e2bb6d1dd00ce17df027bd4eb14433e1c52ac0
IsabellaRV <isabella.ramos-valeriano@stu.ocsb.ca>
2024-10-08 11:15:18 -0400

    math :(

--- main.js
+++ main.js
@@ -19,3 +19,36 @@ console.log(`I generated the number ${random_number}`)
 
 // ***  Your code goes below  ***
 
+let celcius = 100
+let fahrenheit = 0
+celcius +=2
+fahrenheit = (celcius * 9/5) + 32 
+console.log(fahrenheit)
+
+// Part 2
+
+fahrenheit = 77
+celcius = (fahrenheit - 32) * 5/9
+console.log(celcius)
+
+// Part 3
+
+console.log(`${77} degrees Fahrenheit is ${celcius} degrees celsius.`)
+
+// Math gives me a headache :(
+
+// Part 4...
+
+let y2 = 0
+let y1 = 5
+let x2 = 6
+let x1 = -4
+m = (y2 - y1)/(x2 - x1)
+console.log(m)
+
+// Part 5 AUUUGHHHH
+
+let r = Math.random() * 10;
+let h = Math.random() * 10;
+let volume = Math.round(Math.PI * r**2 * h)
+console.log(`The volume of my random cylinder is ${volume}`)
